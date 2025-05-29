a786ee6e9902e78c59108567068c1ed200d055e8
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-05-29 09:50:02 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExploringSQLServerBackupTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
